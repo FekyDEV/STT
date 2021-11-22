f99012c8eb0d27dff50c7ed81406b6d0123ae1c3
Fekysovcze <hlias.sk51@gmail.com>
2021-11-22 18:45:22 +0100

    Updated Info command

--- commands/info.js
+++ commands/info.js
@@ -1,10 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, MessageButton } = require('discord.js');
-const Discord = require("discord.js");
+const { MessageActionRow, MessageButton, Permissions, MessageEmbed, UserFlags } = require('discord.js');
 //
-const bot_name = 'Stop The Trollers'
-const bot_logo = 'https://i.imgur.com/D0FjS3H.png';
-const bot_version = 'v1.5' // AKTUÁLNA VERZIA
 const bot_color = '0x370595';
 //
 const badges = require("discord-badges"); // DETECT USER BADGES
@@ -28,7 +24,6 @@ module.exports = {
 
     async execute(interaction) {
        const user2 = interaction.options.getUser('mention')
-       const user3 = user2.id
 
        console.log(user2)
        //
@@ -58,13 +53,28 @@ module.exports = {
                 result += `${response[i].url}\n`;
               }
         //
-            /*const user_permisssions = []
-            if(user3.permissions.has("ADMINISTRATOR")) {
-                user_permisssions.push("Administrator !")
+            /*let user_permisssions = ""
+              console.log(user2)
+            if(user2.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
+                user_permisssions = "Administrator"
+            } else {
+                user_permisssions = "User"
             }*/
+            //const member = interaction.options.getUser('mention')
+            //const member = interaction.user
+
         //
-        const info_embed = new Discord.MessageEmbed()
-        .setAuthor(user2.tag, user2.displayAvatarURL({ dynamic:true }))
+        let is_bot = ""
+      if(user2.bot) {
+        is_bot = "BOT"
+        console.log("User is BOT")
+      } else {
+        is_bot = "USER"
+        console.log("User isn't BOT !")
+      }
+      // 
+        const info_embed = new MessageEmbed()
+        .setAuthor(user2.tag + ' | ' + is_bot , user2.displayAvatarURL({ dynamic:true }))
         .setColor(0x36393F)
         .setDescription("> <:stt_beta:899703562316181526>")
         .addField("<:stt_id:903032294590255106> ID:", "`\`\`" + user2.id + "\`\`\`")
@@ -74,7 +84,8 @@ module.exports = {
         .addField("<:stt_members:894987766012706877> Created at:", "`\`\`" + `- ${tagcreated} days ago\n` + `- ${tagcreationDate_date}\n` + `- ${tagcreationDate_hours}` + "`\`\`", true)
         .addField("<:stt_invite:896337630315294742> Joined at: <:stt_soon:899704071508877313>", "`\`\`" + `- \n` + `- \n` + `- ` + "`\`\`", true)
         .addField("Badges", "> " + result)
-        .setFooter(`${bot_name}`, `${bot_logo}`)
+        //.addField("<:stt_shield:894863362658283550> 2FA", user2.mfaEnabled ? "<:Yes:871419569426804796>" : "<:No:871419569472933918>")
+        //.setFooter(`${bot_name}`, `${bot_logo}`)
         .setTimestamp();
           //
             interaction.reply({
