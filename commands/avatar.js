85006b7b3a44096afe2dd4bea1120c45957a8bfa
Fekysovcze <hlias.sk51@gmail.com>
2021-11-22 17:45:26 +0100

    Updated Avatar command

--- commands/avatar.js
+++ commands/avatar.js
@@ -15,25 +15,30 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("avatar")
         .setDescription("Show user avatar.")
-        .addUserOption(option =>
-            option.setName('mention')
-                .setDescription('Mention a user')
-                .setRequired(true))
-                
-                .addBooleanOption(option => option
-                    .setName('hide')
-                    .setDescription('Do you want to hide our interaction ?')
-                    
-                ),   
-
+        .addUserOption(option => option
+            .setName('mention')
+            .setDescription('Mention a user')
+            .setRequired(true)) 
+        .addBooleanOption(option => option
+            .setName('hide')
+            .setDescription('Do you want to hide our interaction ?')
+        ),   
     async execute(interaction) {
        const user2 = interaction.options.getUser('mention')
-
+       let avatar_type = ""
+       let is_gif = ""
+    //
+       if(interaction.user.displayAvatarURL({ dynamic: true }).includes(".gif")){
+        avatar_type = ".gif"
+        is_gif = "<:Yes:871419569426804796>"
+       } else {
+        avatar_type = ".webp"
+        is_gif = "<:No:871419569472933918>"
+       }
+    //
         const info_embed = new Discord.MessageEmbed()
-        .setTitle("**LINK**")
-        .setURL(user2.avatarURL({ dynamic: true }))
         .setColor(bot_color)  
-        .setDescription(user2.tag + "'s avatar.")
+        .setDescription('> ' + user2.tag + "'s avatar. \n \n **Gif ?** " + is_gif)
         .setImage(user2.avatarURL({ dynamic: true }))
         .setFooter(`${user2.tag}`, user2.displayAvatarURL({ dynamic: true }))
         .setTimestamp();
@@ -41,11 +46,12 @@ module.exports = {
           const row = new MessageActionRow()
 			.addComponents(
 				new MessageButton()
-                .setURL('https://cdn.discordapp.com/avatars/' + user2.id + '/' + user2.avatar + '.webp')
-				.setLabel('User Avatar') // OBSAH SPRAVY
-				.setStyle('LINK'), // TYP TLACITKA
+                .setURL('https://cdn.discordapp.com/avatars/' + user2.id + '/' + user2.avatar + avatar_type)
+				.setLabel('LINK')
+				.setStyle('LINK'), 
                 //
 			);
+            //
             interaction.reply({
                 embeds: [ info_embed ],
                 components: [row],
