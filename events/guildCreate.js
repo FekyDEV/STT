597f3d52cfedec644528b41f6bccc72a5e9cc7b8
Fekysovcze <hlias.sk51@gmail.com>
2021-11-23 17:15:37 +0100

    Fixed guildCreate & guildDelete

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,61 +1,66 @@
 const axios = require('axios');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Client } = require('discord.js');
+//
+const bot_color = '0x370595';
+const bot_logo = 'https://i.imgur.com/D0FjS3H.png';
 //
 module.exports = {
 	name: 'guildCreate',
 	async execute(guild, client) {
+        //
         let headersList5 = {
             "Accept": "*/*",
             "User-Agent": "Thunder Client (https://www.thunderclient.io)",
-            "Authorization": "Bot ODgwMDQ3NzQ4NjA1NDQ0MTU3.YSYmVQ.mnDEfwBTaoJqT53U6vGbb6F11GI" 
+            "Authorization": "Bot OTA0MDk0ODQzNTAzMDEzOTQ4.YX2h7w.neJLknzp0IdkqF6YodVca5OvbL8" 
            }
            let reqOptions5 = {
-             url: "https://discord.com/api/users/" + guild.ownerID,
+             url: "https://discord.com/api/v9/users/" + guild.ownerId,
              method: "GET",
              headers: headersList5,
            }
            axios.request(reqOptions5).then(function (res) {
              console.log(res.data);
           //
-          const welcome_embed = new Discord.MessageEmbed()
+          const welcome_embed = new MessageEmbed()
           .setColor(`${bot_color}`)
           .setThumbnail(`${bot_logo}`)
           .setTitle('Thank you for adding me to your server !')
           .setDescription('`Useful commands:`')
-          .addField(`${prefix}help`, '> Show all commands.')
-          .addField(`${prefix}setup`, '> Show all setup commands.')
-          .addField(`${prefix}inspect <userID>`, '> Checks if the user is blacklisted.')
-          //.addField(`${prefix}advice`, '> Show random useful advice for server owner. **-SOON**')
-          .setFooter(`${bot_name}`, `${bot_logo}` )
+          .addField(`/help`, '> Show all commands.')
+          .addField(`/setup`, '> Show all setup commands.')
+          .addField(`/inspect <userID>`, '> Checks if the user is blacklisted.')
+          //.addField(`/advice`, '> Show random useful advice for server owner. **-SOON**')
+          .setFooter(client.user.tag, client.user.displayAvatarURL( { dynamic: true } ))
           .setImage('https://i.imgur.com/TlNvTNW.png')
           .setTimestamp()
           //
-          const welcome__support_server_embed = new Discord.MessageEmbed()
+          const welcome__support_server_embed = new MessageEmbed()
           .setColor(`${bot_color}`)
           .setDescription(`**SUPPORT SERVER:** https://bit.ly/stopthetrollers`)
           //
-          const welcome__permissions_advice_embed = new Discord.MessageEmbed()
+          const welcome__permissions_advice_embed = new MessageEmbed()
           .setColor(`${bot_color}`)
           .setDescription(`For a fully functional BOT, we recommend leaving the ` + '`ADMINISTRATOR`' + ` permissions.`)
           //
-          const welcome_log_embed = new Discord.MessageEmbed()
-          .setColor(`${green_color}`)
+          const welcome_log_embed = new MessageEmbed()
+          .setColor(`GREEN`)
           .setDescription(`:white_check_mark:  Guild added: **${guild.name}**  [` + '`' + `${res.data.id}` + '`' + ` / ` + '`' + `${res.data.username + '#' + res.data.discriminator}` + '`' + `] [${guild.memberCount}]`)
           .setFooter(`React ❌ to remove bot from this guild. ` )
           .setTimestamp()
           //
           if(!guild.systemChannel) {
             console.log('No system channel in ' + guild.name) 
-            client.users.cache.get(guild.ownerID).send(welcome_embed); 
-            client.users.cache.get(guild.ownerID).send(welcome__support_server_embed); 
-            client.users.cache.get(guild.ownerID).send(welcome__permissions_advice_embed)
+            //console.log("---> " + client.users.cache.get(guild.ownerId))
+            //client.users.cache.get(guild.ownerId).send(welcome_embed); 
+            //client.users.cache.get(guild.ownerId).send(welcome__support_server_embed); 
+            //client.users.cache.get(guild.ownerId).send(welcome__permissions_advice_embed)
             try {
               axios({
                 method: 'post',
                 url: 'http://95.156.227.203:5000/servers/',
                 data: {
                   sid: guild.id,
-                  owner_id: guild.ownerID,
+                  owner_id: guild.ownerId,
                   owner_tag: res.data.username + "#" + res.data.discriminator
                 }})
                 console.log("Saving data success !")
@@ -63,25 +68,30 @@ module.exports = {
             console.log(err)
               }
               return;
-          } 
-          guild.systemChannel.send(`<@${guild.ownerID}>`);
-          guild.systemChannel.send(welcome_embed);
-          guild.systemChannel.send(welcome__permissions_advice_embed);
-          guild.systemChannel.send(welcome__support_server_embed);
-          client.channels.cache.get('870001541879316490').send(welcome_log_embed)//.then(function (message) {message.react("❌") })
+          } else {
+          guild.systemChannel.send({ content: `<@${guild.ownerId}>`, embeds: [welcome_embed, welcome__permissions_advice_embed, welcome__support_server_embed]});
+
+          //
+         axios.post('https://canary.discord.com/api/webhooks/912699228810272788/_oe9p6G6KxI64vN00gG6_t5AbENpTQ_5kU9KY2MKrCIYIDC9Ua91tZuJBaujXQwae6o0', {
+            username: "STT | LOG",
+            avatar_url: client.user.displayAvatarURL( { dynamic: true}),
+	        embeds: [ welcome_log_embed ],
+        })
          try {
             axios({
               method: 'post',
               url: 'http://95.156.227.203:5000/servers/',
               data: {
                 sid: guild.id,
-                owner_id: guild.ownerID,
+                owner_id: guild.ownerId,
                 owner_tag: res.data.username + "#" + res.data.discriminator
               }})
               console.log("Saving data success !")
               } catch (err) {
           console.log(err)
             }
+        }
           })
+          
 	},
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,9 +1,12 @@
 const axios = require('axios');
 const { MessageEmbed } = require('discord.js');
 //
+const bot_color = '0x370595';
+//
 module.exports = {
 	name: 'guildDelete',
 	async execute(guild, client) {
+        //
         try {
             axios({
               method: 'delete',
@@ -16,10 +19,10 @@ module.exports = {
           let headersList6 = {
             "Accept": "*/*",
             "User-Agent": "Thunder Client (https://www.thunderclient.io)",
-            "Authorization": "Bot ODgwMDQ3NzQ4NjA1NDQ0MTU3.YSYmVQ.mnDEfwBTaoJqT53U6vGbb6F11GI" 
+            "Authorization": "Bot OTA0MDk0ODQzNTAzMDEzOTQ4.YX2h7w.neJLknzp0IdkqF6YodVca5OvbL8" 
            }
            let reqOptions6 = {
-             url: "https://discord.com/api/users/" + guild.ownerID,
+             url: "https://discord.com/api/v9/users/" + guild.ownerId,
              method: "GET",
              headers: headersList6,
            }
@@ -27,13 +30,17 @@ module.exports = {
              console.log(res.data);
           //
           
-          const bye_log_embed = new Discord.MessageEmbed()
-          .setColor(`${red_color}`)
+          const bye_log_embed = new MessageEmbed()
+          .setColor("RED")
           .setDescription(`:no_entry:   Guild removed: **${guild.name}**  [` + '`' + `${res.data.id}` + '`' + ` / ` + '`' + `${res.data.username + '#' + res.data.discriminator}` + '`' + `] [${guild.memberCount}]`)
-          .setFooter(`${bot_name}`, `${bot_logo}` )
+          .setFooter(client.user.tag, client.user.displayAvatarURL( { dynamic: true } ))
           .setTimestamp()
           //
-          client.channels.cache.get('870001541879316490').send(bye_log_embed)
+         axios.post('https://canary.discord.com/api/webhooks/912699228810272788/_oe9p6G6KxI64vN00gG6_t5AbENpTQ_5kU9KY2MKrCIYIDC9Ua91tZuJBaujXQwae6o0', {
+            username: "STT | LOG",
+            avatar_url: client.user.displayAvatarURL( { dynamic: true}),
+	        embeds: [ bye_log_embed ],
+        })
            })
 	},
 };
