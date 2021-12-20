6099b92e1572d402437093a7f35c9bda43d9c0fa
Fekysovcze <hlias.sk51@gmail.com>
2021-12-20 19:10:15 +0100

    Big Update (index & reportuser)

--- commands/reportuser.js
+++ commands/reportuser.js
@@ -1,5 +1,12 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Client, Intents, Collection, Discord, MessageActionRow, MessageButton, WebhookClient, MessageEmbed } = require('discord.js');
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ]
+});
 //
 const config = require("../Data/config.json");
 const bot_logo = 'https://i.imgur.com/D0FjS3H.png';
@@ -8,6 +15,7 @@ const moment = require('moment');
 
 const report = require('../db/report_schema.js');
 const user_profile = require('../db/user_schema.js');
+const { getPriority } = require("os");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -33,13 +41,66 @@ module.exports = {
     
         getUser(int.user) // CHECKING IF USER'S PROFILE IS CREATED...
 
-        //const data = await getReport(int.options.getString('userid'));
-        
+        const data = await getReport(int.options.getString('userid'))
+            
+       if(!data) {
+            console.log("No data..")
+            createReport(int.options.getString('userid'))
 
-        getReport(int.user)
+            const confirm_embed = new MessageEmbed()
+        .setColor("YELLOW")
+        .setAuthor(int.user.tag, int.user.displayAvatarURL( { dynamic: true }))
+        .setTitle("WARNING !")
+        .setTimestamp()
+        .setFooter("<t:123>", bot_logo)
+        .setDescription('> You confirm that the information and proof you submitted is correct and not edited in any way. \n \n > You confirm that the images you submitted are fullscreen images \n \n __Breaking this will lead to a permanent blacklist.__ ')
+        //
+        const confirm_button = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+                .setCustomId('confirm_button')
+				.setLabel('Confirm') // OBSAH SPRAVY
+				.setStyle('SUCCESS'), // TYP TLACITKA
+                //
+            new MessageButton()
+                .setCustomId('rozdelovaci_button')
+				.setLabel('/') // OBSAH SPRAVY
+				.setStyle('SECONDARY') // TYP TLACITKA
+                .setDisabled(true),
+                //
+            new MessageButton()
+                .setCustomId('cancel_button')
+				.setLabel('Cancel') // OBSAH SPRAVY
+				.setStyle('DANGER'), // TYP TLACITKA
+                //
+          );
+          int.reply({
+            embeds: [ confirm_embed ],
+            components: [confirm_button],
+            ephemeral: true
+        })
 
-        async function getReport(usr) {
-            let doc = await report.findOne({ reportedUserID: { $eq: usr.id } });
+       } else {
+        console.log("Data: %s", data)
+        console.log("Satus: %s", data.status)
+
+        if(data.status == "pending") {
+            int.reply({content:'User is already Reported !'})
+                return;
+        } else if(data.status == "accepted") {
+            int.reply({content:'User is already Blacklisted !'})
+                return;
+        }
+    }
+       
+// FUNKCIE
+    async function getReport(usr_id) {
+        let doc = await report.findOne({ reportedUserID: { $eq: usr_id } });
+        return doc;
+    }
+
+        async function createReport(usr) {
+            let doc = await report.findOne({ reportedUserID: { $eq: usr } });
             if (!doc) {
                 const random_id = report_makeid()
                 const report_date = moment().utcOffset(+1).format("DD/MM/YYYY - HH:mm")
@@ -47,28 +108,16 @@ module.exports = {
                 await report.create({
                     status: "pending",
                     reportID: random_id,
-                    reportedUserID: usr.id,
-                    userID: int.options.getString('userid'),
+                    userID: int.user.id,
+                    reportedUserID: int.options.getString('userid'),
                     reportMessage: int.options.getString('message'),
                     reportProof: int.options.getString('proof'),
                     timeCreated: report_date
                 });
-                console.log("Creating new user...")
-            } else {
-                if(doc.status == "pending") {
-                    int.reply({content:'User is already Reported !'})
-                }
-                if(doc.statis == "accepted") {
-                    int.reply({content:'User is already Blacklisted !'})
-                }
-                console.log("no no..")
             }
-            usr.report = doc;
             return doc;
         }
 
-        
-    
     }
 }
 
@@ -82,7 +131,63 @@ function report_makeid() {
     return text;
   }
 
+  async function editReport(usr_id, tochange, newData) {
+    const doc = await getReport(usr_id);
+    switch (tochange) {
+        case 0: { // status
+            await doc.updateOne({ enabled: newData });
+            usr_id.report.enabled = newData;
+        }
+            break;
+        case 1: { // admin
+            await doc.updateOne({ channel: newData });
+            usr_id.report.channel = newData;
+        }
+            break;
+        case 2: { // adminComment
+            await doc.updateOne({ text: newData });
+            usr_id.report.text = newData
+        }
+            break;
+        case 3: { // timeChanged
+            await doc.updateOne({ mention: newData });
+            usr_id.report.mention = newData
+        }
+            break;
+        default:
+            throw new Error("Nope");
+    }
+    return true;
+}
 
+async function editUser(usr_id, tochange, newData) {
+    const doc = await getUser(usr_id);
+    switch (tochange) {
+        case 0: { // have_report
+            await doc.updateOne({ enabled: newData });
+            usr_id.user_profile.enabled = newData;
+        }
+            break;
+        case 1: { // all_reports
+            await doc.updateOne({ channel: newData });
+            usr_id.user_profile.channel = newData;
+        }
+            break;
+        case 2: { // aproved_reports
+            await doc.updateOne({ text: newData });
+            usr_id.user_profile.text = newData
+        }
+            break;
+        case 3: { // denied_reports
+            await doc.updateOne({ mention: newData });
+            usr_id.user_profile.mention = newData
+        }
+            break;
+        default:
+            throw new Error("Nope");
+    }
+    return true;
+}
 
 async function getUser(usr) {
     let doc = await user_profile.findOne({ user_id: { $eq: usr.id } });
@@ -93,6 +198,89 @@ async function getUser(usr) {
         });
         console.log("Creating new user...")
     }
-    usr.user_profile = doc;
     return doc;
 }
+
+async function getReportedUser(usr) {
+    let doc = await report.findOne({ userID: { $eq: usr } });
+    return doc;
+}
+
+client.on("interactionCreate", async (int) => {
+    const user_data = await getUser(int.user)
+    const report_data = await getReportedUser(int.user.id)
+    console.log("0")
+let headersList1 = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.io)",
+    "Authorization": "Bot ODgwMDQ3NzQ4NjA1NDQ0MTU3.YSYmVQ.mnDEfwBTaoJqT53U6vGbb6F11GI" 
+   }
+   let reqOptions1 = {
+    url: "https://discord.com/api/users/" + report_data.reportedUserID,
+     method: "GET",
+     headers: headersList1,
+   }
+   axios.request(reqOptions1).catch((err) => {
+    int.reply("❌ Provide a valid user ID !")
+    if(err) return;
+  })
+   .then(function (user_discord) {
+  // VERFIY CHECK
+    let is_verified = false
+    if(user_data.bdg_verify == true) {
+        is_verified = "<:stt_verified:896337630663409704>"
+      }
+// User //
+    // CONFIRM
+    if(int.customId === "confirm_button"){
+        console.log("1")
+        const report_log_embed = new MessageEmbed()
+              .setColor(`RED`)
+                .setTitle("NEW Report ! | ID: " + report_data.reportID)
+                .setDescription(`<:stt_ticket:894863362503110678> **User Name**⠀⠀⠀⠀⠀<:stt_id:903032294590255106> **User ID** \n ${int.user.tag} ${is_verified}⠀⠀⠀⠀⠀||${int.user.id}||`)
+                .setThumbnail("https://cdn.discordapp.com/avatars/" + user_discord.data.id + "/" + user_discord.data.avatar + ".png")
+                .setImage('https://i.imgur.com/TlNvTNW.png')
+                //.addField("<:stt_ticket:894863362503110678> User Name", `${report_data.user}`,true)
+                //.addField("<:stt_id:903032294590255106> User ID", `||${report_data.userID} ||`,true)
+                .addField("<:stt_ticket:894863362503110678> Reported User Name", `${report_data.reportedUser}`,true)
+                .addField("<:stt_id:903032294590255106> Reported User ID", `||${report_data.reportedUserID} ||`,true)
+                .addField("<:stt_shop:896337630378205196>  Report Message", `${report_data.reportMessage} \n \n ${report_data.reportProof}`, false)
+                .setFooter(`Date: ${report_data.timeCreated}`, 'https://cdn.discordapp.com/emojis/905529168953999371.png?size=96' )
+                .setTimestamp()
+            //
+            const rozhodnutie_reporta = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setCustomId('report_accept')
+                    .setLabel('Accept') // OBSAH SPRAVY
+                    .setStyle('SUCCESS'), // TYP TLACITKA
+                    //
+                    new MessageButton()
+                    .setCustomId('nothiiing')
+                    .setLabel('/') // OBSAH SPRAVY
+                    .setDisabled(true)
+                    .setStyle('SECONDARY'), // TYP TLACITKA
+                    //
+                    new MessageButton()
+                    .setCustomId('report_deny')
+                    .setLabel('Deny') // OBSAH SPRAVY
+                    .setStyle('DANGER'), // TYP TLACITKA
+                    //
+                );
+
+                client.channels.fetch('913364277522468866')
+                .then(channel => channel.send({ content: report_data.reportID, embeds: [ report_log_embed ], components: [ rozhodnutie_reporta ]}).catch(console.error))
+                .catch(console.error);
+
+    } else if(int.customId === "cancel_button"){
+
+    }
+// Staff
+    if(int.customId === "report_accept"){
+
+    } else if(int.customId === "report_deny"){
+
+    } 
+   }) // END (DISCORD API)
+})
+
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest'); // OVERENE
 const { Routes } = require('discord-api-types/v9'); // OVERENE
-const { Discord, Client, Intents, Collection, MessageSelectMenu,MessageActionRow } = require("discord.js");
+const { Discord, Client, Intents, Collection, MessageSelectMenu,MessageActionRow, MessageEmbed, MessageButton } = require("discord.js");
 const client = new Client({
 		intents: [
 			Intents.FLAGS.GUILDS,
@@ -11,6 +11,7 @@ const client = new Client({
 		]
 	});
 const config = require("./Data/config.json");
+const axios = require('axios')
 
 // MONGODB LOAD (From Sloth)
 async function loadDB() {
@@ -151,7 +152,7 @@ if(interaction.customId == "select-profile") {
         });
     }
 }
-    
+
 // OTHER STUFF
     if(!interaction.isCommand()) return;
 
@@ -170,6 +171,190 @@ if(interaction.customId == "select-profile") {
         });
     }
 });
+
+client.on('interactionCreate', async (interaction) => {
+    const report = require('./db/report_schema.js');
+        const user_profile = require('./db/user_schema.js');
+
+        async function getReportedUser(usr) {
+            let doc = await report.findOne({ userID: { $eq: usr } });
+            return doc;
+        }
+
+        async function deleteReport(usr) {
+        const doc4delete = await getReportedUser(usr);
+        doc4delete.deleteOne();
+    }
+// User //
+    // CONFIRM
+    if(interaction.customId === "confirm_button"){
+
+        
+
+async function getUser(usr) {
+    let doc = await user_profile.findOne({ user_id: { $eq: usr.id } });
+    if (!doc) {
+        doc = await user_profile.create({
+            user_id: usr.id,
+            bdg_early: true,
+        });
+        console.log("Creating new user...")
+    }
+    return doc;
+}
+    const user_data = await getUser(interaction.user)
+    const report_data = await getReportedUser(interaction.user.id)
+    console.log(report_data)
+
+    console.log("0")
+    let headersList1 = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.io)",
+    "Authorization": "Bot OTA0MDk0ODQzNTAzMDEzOTQ4.YX2h7w.neJLknzp0IdkqF6YodVca5OvbL8" 
+   }
+   let reqOptions1 = {
+    url: "https://discord.com/api/users/" + report_data.reportedUserID,
+     method: "GET",
+     headers: headersList1,
+   }
+   axios.request(reqOptions1).catch((err) => {
+    interaction.reply("❌ Provide a valid user ID !")
+    if(err) return;
+  })
+   .then(function (user_discord) {
+  // VERFIY CHECK
+    let is_verified = ""
+    if(user_data.bdg_verify == true) {
+        is_verified = "<:stt_verified:896337630663409704>"
+      }
+
+      const report_user_embed = new MessageEmbed()
+      .setColor(`GREEN`)
+      .setDescription("<:stt_check:894982664820506644>  __Your Report has been sended successfully__ !")
+      .setThumbnail("https://cdn.discordapp.com/avatars/" + user_discord.data.id + "/" + user_discord.data.avatar + ".png")
+      .setImage('https://i.imgur.com/TlNvTNW.png')
+      .addField("<:stt_ticket:894863362503110678> User Name", `${user_discord.data.username}#${user_discord.data.discriminator}`,true)
+      .addField("<:stt_id:903032294590255106> User ID", `||${report_data.reportedUserID} ||`,true)
+      .addField("<:stt_shop:896337630378205196>  Report Message", `${report_data.reportMessage} \n \n > Now you have to wait for Staff respond !`)
+      .setFooter(`Status: Pending`, 'https://cdn.discordapp.com/emojis/905529168953999371.png?size=96' )
+      .setTimestamp()
+    //
+          if(report_data.reportProof.includes("https://")) {
+      const report_link = new MessageActionRow()
+      .addComponents(
+          new MessageButton()
+          .setURL(report_data.reportProof)
+          .setLabel('Proof Link') // OBSAH SPRAVY
+          .setStyle('LINK'), // TYP TLACITKA
+          //
+      );
+      interaction.update({
+          embeds: [ report_user_embed ],
+          components: [report_link],
+          ephemeral: true
+      })
+
+
+        const report_log_embed = new MessageEmbed()
+              .setColor(`BLUE`)
+                .setTitle("NEW Report ! | ID: " + report_data.reportID)
+                .setDescription(`<:stt_ticket:894863362503110678> **User Name**⠀⠀⠀⠀⠀<:stt_id:903032294590255106> **User ID** \n ${interaction.user.tag} ${is_verified}⠀⠀⠀⠀⠀||${interaction.user.id}||`)
+                .setThumbnail("https://cdn.discordapp.com/avatars/" + user_discord.data.id + "/" + user_discord.data.avatar + ".png")
+                .setImage('https://i.imgur.com/TlNvTNW.png')
+        
+                .addField("<:stt_ticket:894863362503110678> Reported User Name", `${user_discord.data.username + "#" + user_discord.data.discriminator}`, true)
+                .addField("<:stt_id:903032294590255106> Reported User ID", `||${report_data.reportedUserID} ||`,true)
+                .addField("<:stt_shop:896337630378205196>  Report Message", `${report_data.reportMessage} \n \n ${report_data.reportProof}`, false)
+                .setFooter(`Date: ${report_data.timeCreated}`, 'https://cdn.discordapp.com/emojis/905529168953999371.png?size=96' )
+                .setTimestamp()
+            //
+            const rozhodnutie_reporta = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setCustomId('report_accept')
+                    .setLabel('Accept') // OBSAH SPRAVY
+                    .setStyle('SUCCESS'), // TYP TLACITKA
+                    //
+                    new MessageButton()
+                    .setCustomId('nothiiing')
+                    .setLabel('/') // OBSAH SPRAVY
+                    .setDisabled(true)
+                    .setStyle('SECONDARY'), // TYP TLACITKA
+                    //
+                    new MessageButton()
+                    .setCustomId('report_deny')
+                    .setLabel('Deny') // OBSAH SPRAVY
+                    .setStyle('DANGER'), // TYP TLACITKA
+                    //
+                );
+
+                client.channels.fetch('913364277522468866')
+                .then(channel => channel.send({ content: report_data.reportID, embeds: [ report_log_embed ], components: [ rozhodnutie_reporta ]}).catch(console.error))
+                .catch(console.error);
+            } else {
+                interaction.update({content:'> The proof must be a link (https://) !', components: [], embeds: []})
+                     deleteReport(interaction.user.id)
+            }
+            }) // END (DISCORD API)
+    } else if(interaction.customId === "cancel_button"){
+
+         deleteReport(interaction.user.id)
+
+        const cancel_embed = new MessageEmbed()
+        .setColor("RED")
+        .setAuthor(interaction.user.tag, interaction.user.displayAvatarURL( { dynamic: true }))
+        .setTitle("Report was Canceled !")
+        .setTimestamp()
+        .setFooter("STT")
+        //
+
+        interaction.update({
+            embeds: [ cancel_embed ],
+            components: [ ],
+            ephemeral: true
+        })
+
+        // Pridanie: total_reports pre Usera
+    }
+// Staff
+    if(interaction.customId === "report_accept"){
+        const accepted_date = moment().utcOffset(+1).format("DD/MM/YYYY - HH:mm:ss");
+
+            // Upravenie: admin_approved_reports, admin_total_reports
+
+            // Upravenie: status -> accepted, admin -> interaction.user.tag, timeChanged -> accepted_date
+
+            // Upravenie: have_report pre užívateľa na -> false, user_approved_reports +1
+
+            const report_accepted_log_embed = new MessageEmbed()
+              .setColor(`GREEN`)
+                .setTitle("Report | ID: " + res_report.data[0].reportID)
+                .setDescription(`<:stt_ticket:894863362503110678> **User Name**⠀⠀⠀⠀⠀<:stt_id:903032294590255106> **User ID** \n ${res_report.data[0].user} ${is_verified}⠀⠀⠀⠀⠀||${res_report.data[0].userID}||`)
+                .setThumbnail("https://cdn.discordapp.com/avatars/" + res_discord.data.id + "/" + res_discord.data.avatar + ".png")
+                .setImage('https://i.imgur.com/TlNvTNW.png')
+                .addField("<:stt_ticket:894863362503110678> Reported User Name", `${res_report.data[0].reportedUser}`,true)
+                .addField("<:stt_id:903032294590255106> Reported User ID", `||${res_report.data[0].reportedUserID} ||`,true)
+                .addField("<:stt_shop:896337630378205196>  Report Message", `${res_report.data[0].reportMessage} \n \n ${res_report.data[0].reportProof}`, false)
+                .setFooter(`Date: ${accepted_date}`, 'https://cdn.discordapp.com/emojis/905529657217146950.png?size=96' )
+                .setTimestamp()
+            //
+            interaction.update({
+              embeds: [  ],
+              components: [ ],
+              content: 'Success!',
+          })
+          //
+          axios.post('https://canary.discord.com/api/webhooks/913364429230440449/0QRPGseKwCwDkt2WGcv0L4_KFpr9g3QBtQZ-xu98YmcjgZwqfHg5ZOBQwX1viuyJz4bO', {
+            username: "STT | LOG",
+            avatar_url: client.user.displayAvatarURL( { dynamic: true}),
+	        embeds: [ report_accepted_log_embed ],
+        })
+
+    } else if(interaction.customId === "report_deny"){
+
+    } 
+   
+})
 
 const user_profile = require("./db/user_schema.js")
 async function getUser(usr) {
