157ce5db07d5e55d90fbde482ba8b1fa0279ca1c
Fekysovcze <hlias.sk51@gmail.com>
2021-12-21 14:51:42 +0100

    Updated Report process

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const client = new Client({
 	});
 const config = require("./Data/config.json");
 const axios = require('axios')
+const moment = require('moment');
 
 // MONGODB LOAD (From Sloth)
 async function loadDB() {
@@ -173,40 +174,32 @@ if(interaction.customId == "select-profile") {
 });
 
 client.on('interactionCreate', async (interaction) => {
+    if(interaction.isCommand()) return;
+    if(interaction.isSelectMenu()) return;
+    //if(interaction.customId !== "confirm_button" || interaction.customId !== "cancel_button" || interaction.customId !== "report_accept" || interaction.customId !== "report_deny") return;
+
+    async function getUser(usr) {
+        let doc = await user_profile.findOne({ user_id: { $eq: usr.id } });
+        if (!doc) {
+            doc = await user_profile.create({
+                user_id: usr.id,
+                bdg_early: true,
+            });
+            console.log("Creating new user...")
+        }
+        return doc;
+    }
+
+    async function getReportedUser(usr) {
+        let doc = await report.findOne({ userID: { $eq: usr } });
+        return doc;
+    }
+
     const report = require('./db/report_schema.js');
         const user_profile = require('./db/user_schema.js');
+        const user_data = await getUser(interaction.user)
+        const report_data = await getReportedUser(interaction.user.id)
 
-        async function getReportedUser(usr) {
-            let doc = await report.findOne({ userID: { $eq: usr } });
-            return doc;
-        }
-
-        async function deleteReport(usr) {
-        const doc4delete = await getReportedUser(usr);
-        doc4delete.deleteOne();
-    }
-// User //
-    // CONFIRM
-    if(interaction.customId === "confirm_button"){
-
-        
-
-async function getUser(usr) {
-    let doc = await user_profile.findOne({ user_id: { $eq: usr.id } });
-    if (!doc) {
-        doc = await user_profile.create({
-            user_id: usr.id,
-            bdg_early: true,
-        });
-        console.log("Creating new user...")
-    }
-    return doc;
-}
-    const user_data = await getUser(interaction.user)
-    const report_data = await getReportedUser(interaction.user.id)
-    console.log(report_data)
-
-    console.log("0")
     let headersList1 = {
     "Accept": "*/*",
     "User-Agent": "Thunder Client (https://www.thunderclient.io)",
@@ -222,6 +215,29 @@ async function getUser(usr) {
     if(err) return;
   })
    .then(function (user_discord) {
+    let headersList2 = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.io)",
+        "Authorization": "Bot OTA0MDk0ODQzNTAzMDEzOTQ4.YX2h7w.neJLknzp0IdkqF6YodVca5OvbL8" 
+       }
+       let reqOptions2 = {
+        url: "https://discord.com/api/users/" + interaction.user.id,
+         method: "GET",
+         headers: headersList2,
+       }
+       axios.request(reqOptions2).catch((err) => {
+        interaction.reply("❌ Provide a valid user ID !")
+        if(err) return;
+      })
+       .then(function (user_discord2) {
+
+        async function deleteReport(usr) {
+        const doc4delete = await getReportedUser(usr);
+        doc4delete.deleteOne();
+    }
+// User //
+    // CONFIRM
+    if(interaction.customId === "confirm_button"){
   // VERFIY CHECK
     let is_verified = ""
     if(user_data.bdg_verify == true) {
@@ -295,7 +311,64 @@ async function getUser(usr) {
                 interaction.update({content:'> The proof must be a link (https://) !', components: [], embeds: []})
                      deleteReport(interaction.user.id)
             }
-            }) // END (DISCORD API)
+            
+
+            // Pridanie: all_reports = + 1, have_reports -> true | pre Usera
+
+             editUser(interaction.user, 0,(user_data.all_reports + 1)); // ADD COUNT OF ALL REPORTS FOR USER
+             editUser(interaction.user, 1, true); // ADD USER REPORT STATUS TO true
+
+            async function getUserforEdit(usr) {
+                let doc = await user_profile.findOne({ user_id: { $eq: usr.id } });
+                usr.user_profile = doc;
+                return doc;
+            }
+
+            async function editUser(usr, tochange, newData) {
+                const doc = await getUserforEdit(usr);
+                switch (tochange) {
+                    case 0: { // all_reports
+                        await doc.updateOne({ all_reports: newData });
+                        usr.user_profile.all_reports = newData;
+                    }
+                        break;
+                    case 1: { // have_reports
+                        await doc.updateOne({ have_report: newData });
+                        usr.user_profile.have_report = newData;
+                    }
+                        break;
+                    case 2: { // accepted_reports
+                        await doc.updateOne({ accepted_reports: newData });
+                        usr.user_profile.accepted_reports = newData
+                    }
+                        break;
+                    case 3: { // denied_reports
+                        await doc.updateOne({ denied_reports: newData });
+                        usr.user_profile.denied_reports = newData
+                    }
+                        break;
+                    // FOR ADMIN
+                        case 4: { // admin_all_reports
+                            await doc.updateOne({ admin_all_reports: newData });
+                            usr.user_profile.admin_all_reports = newData
+                        }
+                            break;
+                            case 5: { // admin_accepted_reports
+                                await doc.updateOne({ admin_accepted_reports: newData });
+                                usr.user_profile.admin_accepted_reports = newData
+                            }
+                                break;
+                            case 6: { // admin_denied_reports
+                                await doc.updateOne({ admin_denied_reports: newData });
+                                usr.user_profile.admin_denied_reports = newData
+                            }
+                                break;
+                    default:
+                        throw new Error("Try it again...");
+                }
+                return true;
+            }
+
     } else if(interaction.customId === "cancel_button"){
 
          deleteReport(interaction.user.id)
@@ -314,27 +387,131 @@ async function getUser(usr) {
             ephemeral: true
         })
 
-        // Pridanie: total_reports pre Usera
+        //getUser(interaction.user)
+        
+        
     }
 // Staff
     if(interaction.customId === "report_accept"){
         const accepted_date = moment().utcOffset(+1).format("DD/MM/YYYY - HH:mm:ss");
+        let is_verified = ""
+        if(user_data.bdg_verify == true) {
+            is_verified = "<:stt_verified:896337630663409704>"
+          }
+
+          async function getReportbyID(rid) {
+            let doc = await report.findOne({ reportID: { $eq: rid } });
+            return doc;
+        }
+            
+        async function editReport(rid, tochange, newData) {
+            const doc = await getReportbyID(interaction.message.content);
+            const report2 = require('./db/report_schema.js');
+            switch (tochange) {
+                case 0: { // status
+                    await doc.updateOne({ status: newData });
+                    //rid.report2.status = newData;
+                }
+                    break;
+                case 1: { // admin
+                    await doc.updateOne({ admin: newData });
+                    //rid.report2.admin = newData;
+                }
+                    break;
+                case 2: { // adminComment
+                    await doc.updateOne({ adminComment: newData });
+                    //rid.report2.adminComment = newData
+                }
+                    break;
+                case 3: { // timeChanged
+                    await doc.updateOne({ timeChanged: newData });
+                    //rid.report2.timeChanged = newData
+                }
+                    break;
+                default:
+                    throw new Error("Nope");
+            }
+            return true;
+        }
+
+        async function getUserforEdit2(usr) {
+            let doc = await user_profile.findOne({ user_id: { $eq: usr.id } });
+            usr.user_profile = doc;
+            return doc;
+        }
+
+        async function editUser2(usr, tochange, newData) {
+            const doc = await getUserforEdit2(usr);
+            switch (tochange) {
+                case 0: { // all_reports
+                    await doc.updateOne({ all_reports: newData });
+                    usr.user_profile.all_reports = newData;
+                }
+                    break;
+                case 1: { // have_reports
+                    await doc.updateOne({ have_report: newData });
+                    usr.user_profile.have_report = newData;
+                }
+                    break;
+                case 2: { // accepted_reports
+                    await doc.updateOne({ accepted_reports: newData });
+                    usr.user_profile.accepted_reports = newData
+                }
+                    break;
+                case 3: { // denied_reports
+                    await doc.updateOne({ denied_reports: newData });
+                    usr.user_profile.denied_reports = newData
+                }
+                    break;
+                // FOR ADMIN
+                    case 4: { // admin_all_reports
+                        await doc.updateOne({ admin_all_reports: newData });
+                        usr.user_profile.admin_all_reports = newData
+                    }
+                        break;
+                        case 5: { // admin_accepted_reports
+                            await doc.updateOne({ admin_accepted_reports: newData });
+                            usr.user_profile.admin_accepted_reports = newData
+                        }
+                            break;
+                        case 6: { // admin_denied_reports
+                            await doc.updateOne({ admin_denied_reports: newData });
+                            usr.user_profile.admin_denied_reports = newData
+                        }
+                            break;
+                default:
+                    throw new Error("Try it again...");
+            }
+            return true;
+        }
+
+        
+        // Upravenie: status -> accepted, admin -> interaction.user.tag, timeChanged -> accepted_date
+         editReport(interaction.message.content, 0, "accepted"); // CHANGE REPORT STATUS 
+         editReport(interaction.message.content, 1, interaction.user.tag); // BY ADMIN
+         editReport(interaction.message.content, 2, "Thanks for your Report !"); // ADMIN COMMENT
+         editReport(interaction.message.content, 3, accepted_date); // BY ADMIN
 
             // Upravenie: admin_approved_reports, admin_total_reports
-
-            // Upravenie: status -> accepted, admin -> interaction.user.tag, timeChanged -> accepted_date
-
+            editUser2(interaction.user, 4, (user_data.admin_all_reports + 1)); // ADD COUNT OF ALL REPORTS FOR ADMIN
+            editUser2(interaction.user, 5, (user_data.admin_accepted_reports + 1)); // ADD COUNT OF ACCEPTED REPORTS FOR ADMIN
+           
             // Upravenie: have_report pre užívateľa na -> false, user_approved_reports +1
+            const user_data2 = getUser(user_discord.data.id)
+            editUser2(user_discord.data.id, 0, (user_data2.all_reports + 1)); // ADD COUNT OF ALL REPORTS FOR ADMIN
+            editUser2(user_discord.data.id, 1, false); // ADD COUNT OF ACCEPTED REPORTS FOR ADMIN
+            editUser2(user_discord.data.id, 2, (user_data2.accepted_reports + 1)); // ADD COUNT OF ACCEPTED REPORTS FOR ADMIN
+           
 
             const report_accepted_log_embed = new MessageEmbed()
               .setColor(`GREEN`)
-                .setTitle("Report | ID: " + res_report.data[0].reportID)
-                .setDescription(`<:stt_ticket:894863362503110678> **User Name**⠀⠀⠀⠀⠀<:stt_id:903032294590255106> **User ID** \n ${res_report.data[0].user} ${is_verified}⠀⠀⠀⠀⠀||${res_report.data[0].userID}||`)
-                .setThumbnail("https://cdn.discordapp.com/avatars/" + res_discord.data.id + "/" + res_discord.data.avatar + ".png")
+                .setTitle("Report | ID: " + report_data.reportID)
+                .setDescription(`<:stt_ticket:894863362503110678> **User Name**⠀⠀⠀⠀⠀<:stt_id:903032294590255106> **User ID** \n ${user_discord2.data.username}#${user_discord2.data.discriminator} ${is_verified}⠀⠀⠀⠀⠀||${user_discord2.data.id}||`)
+                .setThumbnail("https://cdn.discordapp.com/avatars/" + user_discord.data.id + "/" + user_discord.data.avatar + ".png")
                 .setImage('https://i.imgur.com/TlNvTNW.png')
-                .addField("<:stt_ticket:894863362503110678> Reported User Name", `${res_report.data[0].reportedUser}`,true)
-                .addField("<:stt_id:903032294590255106> Reported User ID", `||${res_report.data[0].reportedUserID} ||`,true)
-                .addField("<:stt_shop:896337630378205196>  Report Message", `${res_report.data[0].reportMessage} \n \n ${res_report.data[0].reportProof}`, false)
+                .addField("<:stt_ticket:894863362503110678> Reported User Name", `${report_data.reportedUser}`,true)
+                .addField("<:stt_id:903032294590255106> Reported User ID", `||${report_data.reportedUserID} ||`,true)
+                .addField("<:stt_shop:896337630378205196>  Report Message", `${report_data.reportMessage} \n \n ${report_data.reportProof}`, false)
                 .setFooter(`Date: ${accepted_date}`, 'https://cdn.discordapp.com/emojis/905529657217146950.png?size=96' )
                 .setTimestamp()
             //
@@ -353,9 +530,10 @@ async function getUser(usr) {
     } else if(interaction.customId === "report_deny"){
 
     } 
-   
+}) // END (DISCORD API)
+   }) // END (D. API #2)
 })
-
+/*
 const user_profile = require("./db/user_schema.js")
 async function getUser(usr) {
     let doc = await user_profile.findOne({ user_id: { $eq: usr.id } });
@@ -364,7 +542,7 @@ async function getUser(usr) {
     }
     user_profile.usr = doc;
     return doc;
-}
+}*/
 
 client.login(config.token);
  
